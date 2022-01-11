70d5d6d28f2ef3ca2d6a87458af315367fc1c1b2
Yurun <admin@yurunsoft.com>
2022-01-11 15:59:01 +0800

    支持 PHP 8.0、PHP 8.1 新增加的类型

--- src/ReflectionUtil.php
+++ src/ReflectionUtil.php
@@ -3,6 +3,7 @@
 namespace Yurun\IDEHelper;
 
 use InvalidArgumentException;
+use ReflectionIntersectionType;
 use ReflectionNamedType;
 use ReflectionType;
 use ReflectionUnionType;
@@ -36,7 +37,7 @@ class ReflectionUtil
                     $typeStr = '\\' . $typeStr;
                 }
             }
-            if ($type->allowsNull())
+            if ($type->allowsNull() && 'mixed' !== $typeStr)
             {
                 return $typeStr . '|null';
             }
@@ -50,14 +51,24 @@ class ReflectionUtil
             $result = [];
             foreach ($type->getTypes() as $subType)
             {
-                $result[] = self::getTypeCode($subType, $className);
+                $result[] = self::getTypeComments($subType, $className);
             }
-            if ($type->allowsNull())
+            if ($type->allowsNull() && !\in_array('mixed', $result))
             {
                 $result[] = 'null';
             }
 
             return implode('|', $result);
+        }
+        elseif ($type instanceof ReflectionIntersectionType)
+        {
+            $result = [];
+            foreach ($type->getTypes() as $subType)
+            {
+                $result[] = self::getTypeComments($subType, $className);
+            }
+
+            return implode('&', $result);
         }
         else
         {
@@ -88,7 +99,7 @@ class ReflectionUtil
                     $typeStr = '\\' . $typeStr;
                 }
             }
-            if ($type->allowsNull())
+            if ($type->allowsNull() && 'mixed' !== $typeStr)
             {
                 return '?' . $typeStr;
             }
@@ -104,16 +115,85 @@ class ReflectionUtil
             {
                 $result[] = self::getTypeCode($subType, $className);
             }
-            if ($type->allowsNull())
+            if ($type->allowsNull() && !\in_array('mixed', $result))
             {
                 $result[] = 'null';
             }
 
             return implode('|', $result);
         }
+        elseif ($type instanceof ReflectionIntersectionType)
+        {
+            $result = [];
+            foreach ($type->getTypes() as $subType)
+            {
+                $result[] = self::getTypeCode($subType, $className);
+            }
+
+            return implode('&', $result);
+        }
         else
         {
             throw new InvalidArgumentException(sprintf('Unknown type %s', \get_class($type)));
         }
+    }
+
+    public static function allowsType(ReflectionType $type, string $checkType, ?string $className = null): bool
+    {
+        if ('' === $checkType)
+        {
+            return false;
+        }
+        if ('null' === $checkType || '?' === $checkType[0])
+        {
+            return $type->allowsNull();
+        }
+        $checkTypes = explode('|', $checkType);
+        if ('?' === $checkTypes[0][0])
+        {
+            $checkTypes[0][0] = substr($checkTypes[0][0], 1);
+        }
+        if ($type instanceof ReflectionNamedType)
+        {
+            $typeStr = $type->getName();
+            if (!$type->isBuiltin())
+            {
+                if ('self' === $typeStr)
+                {
+                    if (null !== $className)
+                    {
+                        $typeStr = $className;
+                    }
+                }
+            }
+
+            return $typeStr === $checkType || \in_array($typeStr, $checkTypes) || is_subclass_of($checkType, $typeStr);
+        }
+        if ($type instanceof ReflectionUnionType)
+        {
+            foreach ($type->getTypes() as $subType)
+            {
+                $typeStr = ltrim(self::getTypeCode($subType, $className), '\\');
+                if ($typeStr === $checkType || \in_array($typeStr, $checkTypes) || is_subclass_of($checkType, $typeStr))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        elseif ($type instanceof ReflectionIntersectionType)
+        {
+            foreach ($type->getTypes() as $subType)
+            {
+                if (!self::allowsType($subType, $checkType, $className))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        throw new InvalidArgumentException(sprintf('Unknown type %s', \get_class($type)));
     }
 }
